c023b9b08a6344af505a068b3da25854eaa31b53
eomsunyoung <kuroich123@naver.com>
2021-10-22 13:25:06 +0900

    myblog

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     # 127.0.0.1:8000/blog
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('<int:post_id>/', views.detail, name='detail'),
+    path('post/create/', views.post_create, name='post_create'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('', views.index),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
